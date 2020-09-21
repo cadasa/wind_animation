2072db412b9a2309648bd1e9235bc7e683715c5d
cadasa <58220254+cadasa@users.noreply.github.com>
2020-09-22 00:02:50 +0200

    nnn

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 const options = {
-    // Required: API key
     key: 'gB4cSUS30RAGtgeokIWAcEs8cgxQP5tz', // REPLACE WITH YOUR KEY !!!
 
-    // Put additional console output
-    verbose: true,
-
-    // Optional: Initial state of the map
+    // Changing Windy parameters at start-up time
+    // (recommended for faster start-up)
     lat: 15.9,
     lon: 108.1,
     zoom: 5,
+
+    timestamp: Date.now() + 3 * 24 * 60 * 60 * 1000,
+
+    hourFormat: '12h',
+
+    // ...etc
 };
 
-// Initialize Windy API
 windyInit(options, windyAPI => {
-    // windyAPI is ready, and contain 'map', 'store',
-    // 'picker' and other usefull stuff
+    const { store } = windyAPI;
+    // All the params are stored in windyAPI.store
 
-    const { map } = windyAPI;
-    // .map is instance of Leaflet map
+    const levels = store.getAllowed('availLevels');
+    // levels = ['surface', '850h', ... ]
+    // Getting all available values for given key
 
-// L.popup()
-    //  .setLatLng([15.8, 108.1])
-    //  .setContent('Bão No. 5 - Noul')
-    //  .openOn(map);
+    let i = 0;
+    setInterval(() => {
+        i = i === levels.length - 1 ? 0 : i + 1;
+
+        // Changing Windy params at runtime
+        store.set('level', levels[i]);
+    }, 500);
+
+    // Observing change of .store value
+    store.on('level', level => {
+        console.log(`Level was changed: ${level}`);
+    });
 });
